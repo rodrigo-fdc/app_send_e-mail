ac0e4ab8df23f83d376868bb9224fd265d87c3d2
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-10-15 23:28:19 -0300

    Projeto Send Email

---
+++ public/processa_envio.php
@@ -0,0 +1,9 @@
+<?php
+
+	require "../bibliotecas/PHPMailer/Exception.php";
+	require "../bibliotecas/PHPMailer/OAuth.php";
+	require "../bibliotecas/PHPMailer/PHPMailer.php";
+	require "../bibliotecas/PHPMailer/POP3.php";
+	require "../bibliotecas/PHPMailer/SMTP.php";
+
+	require "../processa_envio.php";
